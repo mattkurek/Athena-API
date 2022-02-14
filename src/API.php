3cb9fd32f795afabdfbd598fa29a523b3c954161
Matt Kurek <me@mattkurek.com>
2022-02-14 05:20:28 +0000

    composer test

---
+++ src/API.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace MattKurek\AthenaAPI;
+
+/**
+ *      used to initiate the web app
+ */
+class API {
+
+    public function __construct(
+        
+    )
+    {
+        
+        echo "API Initiation Beginning <br />";
+
+        // first we need to decipher the request
+
+        // then we need to load the proper endpoint
+
+        echo "API Initiation Finished <br />";
+    }
+
+    public function __destruct()
+    {
+        
+
+
+    }
+
+}
